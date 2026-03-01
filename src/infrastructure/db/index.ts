a41e77cdfe065bbf3ecc9dc847546db04225ca70
ricotandrio <ricotandrio@gmail.com>
2026-03-01 17:08:28 +0700

    refactor: reorganize user mapping repository structure and remove deprecated file

--- src/infrastructure/db/index.ts
+++ src/infrastructure/db/index.ts
@@ -1,5 +1,5 @@
 import { initDb } from './client';
 import { DbGuildRepository } from './repositories/DbGuildRepository.repository';
-import { DbUserMappingRepository } from './repositories/DbUserMapping.repository';
+import { DbUserMappingRepository } from './repositories/DbUserMappingRepository.repository';
 
 export { initDb, DbGuildRepository, DbUserMappingRepository };

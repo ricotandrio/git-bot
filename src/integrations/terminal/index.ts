877b91c2bf7a645da2aa27049d68d537c2650846
ricotandrio <ricotandrio@gmail.com>
2026-04-05 22:40:44 +0700

    feat: add Terminal integration with command execution capabilities

---
+++ src/integrations/terminal/index.ts
@@ -0,0 +1,7 @@
+import { TerminalIntegration } from './terminal.integration';
+
+export * from './terminal.types';
+export * from './terminal.client';
+export * from './terminal.integration';
+
+export const terminalIntegration = new TerminalIntegration();
